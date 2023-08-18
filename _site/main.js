01d09c91584d068ace0f36c4b581a42a586946ab
jarciniegas20 <jarciniegas68w@gmail.com>
2023-08-17 19:32:14 -0700

    Improving sound

--- _site/main.js
+++ _site/main.js
@@ -1,13 +1,3 @@
-// const clickButton = document.getElementById('clickButton');
-// const clickSound = new Howl({
-//   src: ['single_click_sound.mp3'],
-//   html5: true
-// });
-
-// clickButton.addEventListener('click', () => {
-//   clickSound.play();
-// });
-
 
 document.addEventListener('DOMContentLoaded', function () {
     const clickButton = document.getElementById('clickButton');
