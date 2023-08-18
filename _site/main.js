8a2e890aea572d2142e859ab07a2cef39dc7eeff
jarciniegas20 <jarciniegas68w@gmail.com>
2023-08-17 19:29:32 -0700

    Improving sound

--- _site/main.js
+++ _site/main.js
@@ -1,8 +1,27 @@
-const clickButton = document.getElementById('clickButton');
-const clickSound = new Howl({
-  src: ['single_click_sound.mp3']
-});
+// const clickButton = document.getElementById('clickButton');
+// const clickSound = new Howl({
+//   src: ['single_click_sound.mp3'],
+//   html5: true
+// });
 
-clickButton.addEventListener('click', () => {
-  clickSound.play();
-});
+// clickButton.addEventListener('click', () => {
+//   clickSound.play();
+// });
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    const clickButton = document.getElementById('clickButton');
+    
+    const clickSound = new Howl({
+      src: ['single_click_sound.mp3'],
+      html5: true
+    });
+    
+    clickButton.addEventListener('click', () => {
+      clickSound.play();
+    });
+  });
+  
+  
+  
+  
